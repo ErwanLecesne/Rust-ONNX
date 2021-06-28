814b4f73a6b2fb404370d221dbb789cf46e81db6
Erwan-sudo <72436205+Erwan-sudo@users.noreply.github.com>
2021-06-28 15:50:59 +0200

    add user input in a better way

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use std::io;
 use std::env;
 use std::path::Path;
 use std::path::PathBuf;
+use ferris_says::*;
 
 fn main() {
 
@@ -18,19 +19,18 @@ fn main() {
     println!("Your current dir is : {}\n",current_dir.display());
 
     println!("Enter the file path.(ex: /folder1/folder2/myfile.nii)\n ");
-    io::stdin().read_line(&mut str_buf).unwrap();
-    
+    io::stdin().read_line(&mut str_buf).unwrap(); 
     let image_str = str_buf.trim().to_owned();
     let image_path = Path::new(&image_str);
    
     let output_path = Path::new(&image_str).parent().unwrap().join("output.nii");
-
     let mut niiwriter = writer::WriterOptions::new(&output_path);
-
     println!("OutPut dir : {}\n",output_path.display());
 
     
     println!("Enter the ONNX path. ex: /folder1/folder2/myfile.onnx)\n ");
+
+    str_buf = String::new();
     io::stdin().read_line(&mut str_buf).unwrap();
     let onnx_str = str_buf.trim().to_owned();
     let onnx_path = Path::new(&onnx_str);
@@ -42,16 +42,13 @@ fn main() {
     let header = image.header();
     let mut volume = image.into_volume().into_ndarray::<f32>().unwrap();
     let dims = volume.shape();
+    /*
     for v in dims.iter() {
         println!("input shape {:?}",v);
     
     }
-   
-
-    //
-    //let mut write = niiwriter.write_nifti(&volume);
+   */
     
-/*
     let start = SystemTime::now();
     volume.swap_axes(0, 3); 
     volume.swap_axes(1, 2); 
@@ -65,7 +62,7 @@ fn main() {
 
     let model = tract_onnx::onnx()
     // load the model
-    .model_for_path(&onnx_path.trim().to_string()).unwrap()
+    .model_for_path(onnx_path).unwrap()
     // specify input type and shape
     .with_input_fact(0, InferenceFact::dt_shape(f32::datum_type(), tvec!(16, 1, 128, 128))).unwrap()
     // optimize the model
@@ -84,29 +81,36 @@ fn main() {
 
 
     let dims_res = prediction.shape();
-
+/*
     
     for v in dims_res.iter() {
         println!("output shape {:?}",v);
     }
-
+*/
 
     casted.swap_axes(0, 2);
 
     
 
     let dims_pred= casted.shape();
-
+/*
     for v in dims_pred.iter() {
         println!("prediction shape  {:?}",v);
     }
-    
-    niiwriter = writer::WriterOptions::new("/Users/Erwan/Downloads/output.nii");
-    write = niiwriter.write_nifti(&casted);
+   */ 
+    niiwriter = writer::WriterOptions::new(output_path);
+    let _wrote = niiwriter.write_nifti(&casted);
 
     let end = SystemTime::now();
-    let difference = end.duration_since(start);
+    let mut difference = end.duration_since(start);
     println!("time to make it {:?}",difference);  
- */   
+   
+
+    let stdout = io::stdout();
+    let out = b"Inference done !";
+    let width = 24;
+
+    let mut writer = io::BufWriter::new(stdout.lock());
+    say(out, width, &mut writer).unwrap();
    
 }
